cb28efc837b88aaeae96f4e859542255b24ad073
Daniel-Cunningham02 <danrcunn@ut.utm.edu>
2023-04-17 15:24:22 -0500

    Basic methods work. Working on more complicated methods

--- heap.cpp
+++ heap.cpp
@@ -1,26 +1,30 @@
 #include "heap.h"
+#include <iostream>
+
 
 heap::heap() {
-
 }
 
 int heap::size() {
-
+  return buffer.size();
 }
 
 
 void heap::insert(int value) {
-
+  
 }
 
 void heap::remove_max() {
-
 }
 
 int heap::max() {
-
+  return buffer[0];
 }
 
 void heap::print() {
-
+  std::cout << "In order:\n"; 
+  for(auto it = buffer.begin(); it != buffer.end(); it++) {
+    std::cout << *it << "\n";
+  }
+  std::cout.flush();
 }
